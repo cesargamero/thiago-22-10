6ebf89d749b9cb2e80669016d36fa04330e589dc
cesargamero <cestp2006@gmail.com>
2024-10-22 22:08:43 -0300

    Add files via upload

---
+++ src/Teste.java
@@ -0,0 +1,10 @@
+package com.exemplo.pacote;
+
+public class Teste {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.setNome("Bobby");
+        animal.fazerSom();
+
+    }
+}
